9d567e5cedb96f8ea71986ae13d47f61fbdbb55a
alojzije <shljunak@hotmail.com>
2015-02-11 19:43:45 +0100

    b

---
+++ videoEffect.js
@@ -0,0 +1,54 @@
+function frameConverter(video,canvas) {
+
+    // Set up our frame converter
+    this.video = video;
+    this.viewport = canvas.getContext("2d");
+    this.width = canvas.width;
+    this.height = canvas.height;
+    // Create the frame-buffer canvas
+    this.framebuffer = document.createElement("canvas");
+    this.framebuffer.width = this.width;
+    this.framebuffer.height = this.height;
+    this.ctx = this.framebuffer.getContext("2d");
+    // Default video effect is blur
+    this.effect = JSManipulate.blur;
+    // This variable used to pass ourself to event call-backs
+    var self = this;
+    // Start rendering when the video is playing
+    this.video.addEventListener("play", function() {
+        self.render();
+      }, false);
+
+
+    // Rendering call-back
+    this.render = function() {
+        if (this.video.paused || this.video.ended) {
+          return;
+        }
+        this.renderFrame();
+        var self = this;
+        // Render every 10 ms
+        setTimeout(function () {
+            self.render();
+          }, 10);
+    };
+
+    // Compute and display the next frame 
+    this.renderFrame = function() {
+        // Acquire a video frame from the video element
+        this.ctx.drawImage(this.video, 0, 0, this.video.videoWidth,
+                    this.video.videoHeight,0,0,this.width, this.height);
+        var data = this.ctx.getImageData(0, 0, this.width, this.height);
+        // Apply image effect
+        this.effect.filter(data,this.effect.defaultValues);
+        // Render to viewport
+        this.viewport.putImageData(data, 0, 0);
+    return;
+    };
+};
+
+// Initialization code
+video = document.getElementById("video");
+canvas = document.getElementById("canvas");
+fc = new frameConverter(video,canvas);
+
